a3d588ad17a204c7a45fb52da111e4a3605a9689
Tamas Gal <tgal@km3net.de>
2017-05-19 03:15:07 +0200

    Add RawHit

--- src/KM3NeT.jl
+++ src/KM3NeT.jl
@@ -1,15 +1,35 @@
 module KM3NeT
 
+using HDF5
+
 export TimesliceHit, rows
 
 
 abstract Hit
+
+immutable RawHit <: Hit
+    channel_id::UInt8
+    time::Int32
+    tot::UInt8
+    dom_id::UInt32
+end
+
+RawHit(hit::HDF5.HDF5Compound{15}) = begin
+    KM3NeT.RawHit(hit.data[1], hit.data[12], hit.data[13], )
+end
 
 immutable TimesliceHit <: Hit
     channel_id::Int8
     time::Int32
     tot::Int16
 end
+
+
+Base.show(io::IO, h::RawHit) = begin
+    print(io, "$(typeof(h)): channel_id($(h.channel_id)), time($(h.time)), " *
+              "tot($(h.tot)), dom_id($(h.dom_id))")
+end
+
 
 rows(x) = (x[i, :] for i in indices(x,1))
 
