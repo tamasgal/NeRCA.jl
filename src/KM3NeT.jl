54fda9c47454853b4fc83ac29a82fd08cc311553
Tamas Gal <tgal@km3net.de>
2017-06-25 19:05:34 +0200

    Update syntax to julia 0.6

--- src/KM3NeT.jl
+++ src/KM3NeT.jl
@@ -6,13 +6,13 @@ export RawHit, TimesliceHit, rows, read_hits
 
 
 # Basic types
-immutable Position{T<:AbstractFloat}
+struct Position{T<:AbstractFloat}
     x::T
     y::T
     z::T
 end
 
-immutable Direction{T<:AbstractFloat}
+struct Direction{T<:AbstractFloat}
     x::T
     y::T
     z::T
@@ -20,14 +20,14 @@ end
 
 
 # Hardware
-immutable PMT
+struct PMT
     channel_id::UInt8
     pos::Position
     dir::Direction
 end
 
 
-immutable DOM
+struct DOM
     id::UInt32
     floor::UInt8
     line::UInt8
@@ -36,9 +36,9 @@ end
 
 
 # Signal
-abstract type Hit
+abstract type Hit end
 
-immutable RawHit <: Hit
+struct RawHit <: Hit
     channel_id::UInt8
     dom_id::UInt32
     time::Int32
@@ -50,7 +50,7 @@ RawHit(hit::HDF5.HDF5Compound{5}) = begin
     RawHit(hit.data...)
 end
 
-immutable TimesliceHit <: Hit
+struct TimesliceHit <: Hit
     channel_id::Int8
     time::Int32
     tot::Int16
