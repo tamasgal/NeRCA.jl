14c32fca587a05729a47c1b3523b67debb6ec56b
Tamas Gal <tgal@km3net.de>
2017-08-11 00:09:29 +0200

    Docs

---
+++ docs/build/search_index.js
@@ -0,0 +1,59 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.jl Package",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#KM3NeT.jl-Package-1",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.jl Package",
+    "category": "section",
+    "text": "..."
+},
+
+{
+    "location": "index.html#KM3NeT.calibrate-Tuple{Array{KM3NeT.RawHit,1},KM3NeT.Calibration}",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.calibrate",
+    "category": "Method",
+    "text": "function calibrate(hits::Vector{RawHit}, calibration::Calibration)\n\nApply geometry and time calibration to given hits.\n\n\n\n"
+},
+
+{
+    "location": "index.html#KM3NeT.nfoldhits-Union{Tuple{Array{T,1},Any,Any}, Tuple{T}} where T<:KM3NeT.Hit",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.nfoldhits",
+    "category": "Method",
+    "text": "function nfoldhits(hits::Vector{T}, Δt, n) where {T<:Hit}\n\nCreate a Vector with hits contributing to n-fold coincidences within a time window of Δt.\n\n\n\n"
+},
+
+{
+    "location": "index.html#KM3NeT.triggered-Union{Tuple{Array{T,1}}, Tuple{T}} where T<:KM3NeT.Hit",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.triggered",
+    "category": "Method",
+    "text": "function triggered(hits::Vector{T}) where {T<:Hit}\n\nReturn a Vector of triggered hits.\n\n\n\n"
+},
+
+{
+    "location": "index.html#KM3NeT.pld3-Tuple{Any,Any,Any}",
+    "page": "KM3NeT.jl Package",
+    "title": "KM3NeT.pld3",
+    "category": "Method",
+    "text": "function pld3(p1, p2, d2)\n\nCalculate the distance between a point (p1) and a line (given by p2 and d2).\n\n\n\n"
+},
+
+{
+    "location": "index.html#Functions-1",
+    "page": "KM3NeT.jl Package",
+    "title": "Functions",
+    "category": "section",
+    "text": "Modules = [KM3NeT]\nPages   = [\"io.jl\", \"rba.jl\"]Modules = [KM3NeT]\nOrder   = [:function, :type]"
+},
+
+]}
