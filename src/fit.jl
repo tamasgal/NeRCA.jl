160f19e07f1c5bdce4b4010444d0976c0d37a3bf
Tamas Gal <tgal@km3net.de>
2019-03-05 19:08:34 +0100

    Fix unit

--- src/fit.jl
+++ src/fit.jl
@@ -4,13 +4,14 @@
 Calculates five parameters to describe a track for a single DU case.
 """
 function single_du_params(track::KM3NeT.Track)
+    const c_ns = c / 1e9
     pos = track.pos
     dir = Direction(normalize(track.dir))
     t₀ = track.time
     proj = pos ⋅ dir
     z_closest = (pos.z - dir.z*proj) / (1 - dir.z^2)
-    t_closest = t₀ + (z_closest * dir.z - proj)/c
-    p_t_closest = pos + c * (t_closest - t₀) * dir
+    t_closest = t₀ + (z_closest * dir.z - proj)/c_ns
+    p_t_closest = pos + c_ns * (t_closest - t₀) * dir
     d_closest = √(p_t_closest[1]^2 + p_t_closest[2]^2)
 
     d_closest, t_closest, z_closest, dir.z, t₀
