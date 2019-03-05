22a2b557cef237ee16b1fcc8a9a24afdbb577c5e
Tamas Gal <tgal@km3net.de>
2019-03-05 18:36:19 +0100

    Fix signature

--- test/mc_tests.jl
+++ test/mc_tests.jl
@@ -3,5 +3,5 @@ using Test
 
 
 # make_cherenkov_calculator()
-ccalc = KM3NeT.make_cherenkov_calculator([0,0,1], [0,0,-1])
+ccalc = KM3NeT.make_cherenkov_calculator(KM3NeT.Track([0,0,-1], [0,0,1], 0))
 @test 3.33564095 ≈ ccalc([0, 0, 0])
