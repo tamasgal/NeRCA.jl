6071b72a0ad32cac06151bb178816a46e85b59d1
Tamas Gal <tgal@km3net.de>
2019-03-10 20:23:21 +0100

    Fix hit compatibility for calibrating

--- src/KM3NeT.jl
+++ src/KM3NeT.jl
@@ -54,8 +54,15 @@ function calibrate(hits::Vector{T}, calibration::Calibration) where {T<:DAQHit}
         time = hit.time + t0
         du = calibration.du[dom_id]
         floor = calibration.floor[dom_id]
+        triggered = false
+        if T === Hit
+            triggered = hit.triggered
+        end
+        if T === DAQTriggeredHit
+            triggered = hit.trigger_mask > 0
+        end
         c_hit = CalibratedHit(channel_id, dom_id, du, floor, time, tot,
-                              pos, dir, t0, hit.triggered)
+                              pos, dir, t0, triggered)
         push!(calibrated_hits, c_hit)
     end
     calibrated_hits
