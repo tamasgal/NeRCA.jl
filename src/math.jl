c2754c5ee6d2268ed94c69af0b6e3f80d67d45be
Tamas Gal <tgal@km3net.de>
2019-05-22 16:19:05 +0200

    Add azimuth

--- src/math.jl
+++ src/math.jl
@@ -8,3 +8,13 @@ function angle_between(v1, v2)
     _v2 = normalize(v2)
     angle = acos(dot(_v1, _v2))
 end
+
+function azimuth(d::Direction)
+    if d.x >= 0 && d.y >= 0  # Quadrant I
+        return atan(d.x/d.y)
+    end
+    if d.x < 0 && d.y >= 0  # Quadrant II
+        return atan(d.x/d.y) + 2π
+    end
+    return atan(d.x/d.y) + π  # Quadrant III and IV
+end
--- test/math_tests.jl
+++ test/math_tests.jl
@@ -8,6 +8,8 @@ using Test
 @test π/2 ≈ KM3NeT.angle_between([1,0,0], [0,0,1])
 @test π ≈ KM3NeT.angle_between([1,0,0], [-1,0,0])
 
-# make_cherenkov_calculator()
-# ccalc = KM3NeT.make_cherenkov_calculator(KM3NeT.Track([0,0,-1], [0,0,1], 0))
-# @test 3.33564095 =≈ ccalc([0, 0, 0])
+# azimuth()
+@test 0 == KM3NeT.azimuth(Direction(0,1,0))
+@test π/2 ≈ KM3NeT.azimuth(Direction(1,0,0))
+@test π ≈ KM3NeT.azimuth(Direction(0,-1,0))
+@test (3/2)π ≈ KM3NeT.azimuth(Direction(-1,0,0))
