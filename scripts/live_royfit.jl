49e7c052d1f15600e4babfd3002a96e91f0959e5
Tamas Gal <tgal@km3net.de>
2019-06-03 18:40:21 +0200

    Add live royfit script

--- scripts/live_royfit.jl
+++ scripts/live_royfit.jl
@@ -6,31 +6,32 @@ using Dates
 using Measures
 GR.inline("png")
 
-if length(ARGS) < 1
-    println("Usage: ./live_royfit.jl DETX")
+if length(ARGS) < 2
+    println("Usage: ./live_royfit.jl DETX TIME_RES")
     exit(1)
 end
 
 
 const calib = KM3NeT.read_calibration(ARGS[1])
+const LIGIER_PORT = parse(Int, ARGS[2])
+const TIME_RES = ARGS[3]
 
 function main()
     println("Starting live ROyFit")
 
-    for message in CHClient(ip"127.0.0.1", 55530, ["IO_EVT"])
+    for message in CHClient(ip"127.0.0.1", LIGIER_PORT, ["IO_EVT"])
         event = KM3NeT.read_io(IOBuffer(message.data), KM3NeT.DAQEvent)
 
         hits = calibrate(event.hits, calib)
         triggered_hits = filter(h->h.triggered, hits)
-        n_dus = length(unique(h->h.du, triggered_hits))
+        dus = sort(unique(map(h->h.du, hits)))
+        n_dus = length(dus)
         n_doms = length(unique(h->h.dom_id, triggered_hits))
 
         if n_doms < 4
             continue
         end
 
-        triggered_hits = filter(h->h.triggered, hits)
-        dus = sort(unique(map(h->h.du, hits)))
         colours = palette(:default)
         plot()
         Q = []
@@ -43,6 +44,7 @@ function main()
             fit = KM3NeT.single_du_fit(du_hits)
             push!(Q, fit.Q)
             plot!(du_hits, fit, markercolor=colours[idx], label="DU $(du)", max_z=calib.max_z)
+            write_time_residuals(TIME_RES, event, du_hits, fit)
         end
         if sum(Q) < 200 && n_doms > 12 && n_dus > 1
             println("Plotting...")
@@ -54,10 +56,24 @@ function main()
 
             plot!(title="$(fit_params)\n$(event_params), $(trigger_params)\n$(time_params)", titlefontsize=8, margin=5mm)
 
-            savefig("ztplot.png")
+            savefig("plots/ztplot_roy.png")
         end
     end
 end
 
+function write_time_residuals(filename, event, hits, fit)
+    if !isfile(filename)
+        fobj = open(filename, "w")
+        write(fobj, "run,timestamp,du,floor,dom_id,t_res,Q\n")
+    else
+        fobj = open(filename, "a")
+    end
+    dγ, ccalc = KM3NeT.make_cherenkov_calculator(fit.sdp)
+    for hit in hits
+        Δt = hit.t - ccalc(hit.pos.z)
+        write(fobj, "$(event.run_id),$(event.timestamp),$(hit.du),$(hit.floor),$(hit.dom_id),$(Δt),$(fit.Q)\n")
+    end
+    close(fobj)
+end
 
 main()
