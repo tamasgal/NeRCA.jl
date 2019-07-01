3469cd36a7dac615cfcc5c11154f98cfb79aca12
Tamas Gal <tgal@km3net.de>
2019-07-01 23:56:30 +0200

    Fix outliers due to floating point inaccuracy

--- src/math.jl
+++ src/math.jl
@@ -6,7 +6,7 @@ Calculates the angle between two vectors
 function angle_between(v1, v2)
     _v1 = normalize(v1)
     _v2 = normalize(v2)
-    angle = acos(dot(_v1, _v2))
+    angle = acos(min(dot(_v1, _v2), 1))
 end
 
 
