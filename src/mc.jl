c9486f4dd647670406f022fddda7e293ceda1469
Tamas Gal <tgal@km3net.de>
2019-07-05 23:33:50 +0200

    Extend mc time converter to deal with rbr daq events

--- src/mc.jl
+++ src/mc.jl
@@ -19,11 +19,11 @@ end
 
 
 """
-    function make_mc_time_converter(event_info::MCEventInfo)
+    function make_mc_time_converter(event_info::Union{MCEventInfo,DAQEventInfo})
 
 Returns a function which converts MC time to JTE time.
 """
-function make_mc_time_converter(event_info::MCEventInfo)
+function make_mc_time_converter(event_info::Union{MCEventInfo,DAQEventInfo})
     function time_converter(time)
         return time - (event_info.timestamp * 1e9 + event_info.nanoseconds) + event_info.mc_time
     end
