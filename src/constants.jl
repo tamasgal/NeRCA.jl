afb9fb1ec7f8c72a1b989b846e9c06070fe4eb33
Tamas Gal <tgal@km3net.de>
2019-07-11 17:57:53 +0200

    Fix constants

--- src/constants.jl
+++ src/constants.jl
@@ -1,7 +1,2 @@
-import PhysicalConstants.CODATA2018: c_0
-
-const c = 2.99792458e8
-const c_water = 2.1744575179e8
-const n_water = c / c_water
-
+const c_0 = 2.99792458e8
 const N_SEAWATER = 1.3800851282
