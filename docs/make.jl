5115e1a106e9dc2351285178368e4aac7545a892
Tamas Gal <tgal@km3net.de>
2019-07-31 16:03:12 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,36 @@ using Pkg
 Pkg.activate(@__DIR__)
 
 using NeRCA
+using Literate
 using Documenter
 
 
+# const src_dir = "src"
+# const docs_src_dir = "docs/src"
+# const api_dir = "api"
+#
+# pages = Vector{Any}()
+#
+# # Generate API docs
+# api_pages = []
+# for file ∈ readdir(src_dir)
+#     if file == "fit.jl"
+#         continue
+#     end
+#     out_path = joinpath(docs_src_dir, api_dir)
+#     Literate.markdown(joinpath(src_dir, file), out_path; documenter=false)
+#     push!(api_pages, joinpath(api_dir, replace(file, ".jl" => ".md")))
+# end
+# push!(pages, "API" => api_pages)
+# print(pages)
+    
+
 makedocs(modules=[NeRCA],
-         doctest=true,
+         # doctest=true,
          format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
          authors = "Tamas Gal",
          sitename = "NeRCA.jl",
-         pages = [
-             "index.md",
-             "Calibration" => "calibration.md",
-             #= "Subsection" => [ =#
-             #=     ... =#
-             #= ] =#
-         ]
+         pages = []
          )
 #=  =#
 #= deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"), =#
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,23 +1,4 @@
 # NeRCA.jl Package
 
-...
-
-## Functions
-
-### I/O
-```@autodocs
-Modules = [NeRCA]
-Pages   = ["io.jl"]
-```
-
-### RainbowAlga
-```@autodocs
-Modules = [NeRCA]
-Pages   = ["rba.jl"]
-```
-
-### Other
-```@autodocs
-Modules = [NeRCA]
-Order   = [:function, :type]
+```@contents
 ```
