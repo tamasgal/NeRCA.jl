8d86d3d12e72c23346518dcb55ba7e35fc9f192c
Tamas Gal <tgal@km3net.de>
2019-10-07 09:39:23 +0200

    Update docs generation

--- docs/make.jl
+++ docs/make.jl
@@ -1,46 +1,17 @@
-using Pkg
-Pkg.activate(@__DIR__)
+using Documenter, NeRCA
 
-using NeRCA
-using Literate
-using Documenter
+makedocs(;
+    modules=[NeRCA],
+     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    pages=[
+        "Home" => "index.md",
+    ],
+    repo="https://github.com/tamasgal/NeRCA.jl/blob/{commit}{path}#L{line}",
+    sitename="NeRCA.jl",
+    authors="Tamas Gal",
+    assets=String[],
+)
 
-
-# const src_dir = "src"
-# const docs_src_dir = "docs/src"
-# const api_dir = "api"
-#
-# pages = Vector{Any}()
-#
-# # Generate API docs
-# api_pages = []
-# for file ∈ readdir(src_dir)
-#     if file == "fit.jl"
-#         continue
-#     end
-#     out_path = joinpath(docs_src_dir, api_dir)
-#     Literate.markdown(joinpath(src_dir, file), out_path; documenter=false)
-#     push!(api_pages, joinpath(api_dir, replace(file, ".jl" => ".md")))
-# end
-# push!(pages, "API" => api_pages)
-# print(pages)
-    
-
-makedocs(modules=[NeRCA],
-         # doctest=true,
-         format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-         authors = "Tamas Gal",
-         sitename = "NeRCA.jl",
-         pages = []
-         )
-#=  =#
-#= deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"), =#
-#=     repo = "github.com/tamasgal/NeRCA.jl.git", =#
-#=     julia  = "0.7.0", =#
-#=     osname = "linux") =#
-#
-
-# if get(ENV, "CI", nothing) == "true"
-deploydocs(repo = "github.com/tamasgal/NeRCA.jl.git",
-           target = "build")
-# end
+deploydocs(;
+    repo="github.com/tamasgal/NeRCA.jl",
+)
