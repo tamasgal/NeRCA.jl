1d138b9736a4b696a870d1f10eca4a2bc6a148c5
Tamas Gal <tgal@km3net.de>
2019-10-07 13:21:31 +0200

    Add math tests

--- test/math_tests.jl
+++ test/math_tests.jl
@@ -2,14 +2,23 @@ using NeRCA
 using Test
 
 
-# angle_between()
-@test 0 == NeRCA.angle_between([1,0,0], [1,0,0])
-@test π/2 ≈ NeRCA.angle_between([1,0,0], [0,1,0])
-@test π/2 ≈ NeRCA.angle_between([1,0,0], [0,0,1])
-@test π ≈ NeRCA.angle_between([1,0,0], [-1,0,0])
+@testset "angle_between()" begin
+    @test 0 == NeRCA.angle_between([1,0,0], [1,0,0])
+    @test π/2 ≈ NeRCA.angle_between([1,0,0], [0,1,0])
+    @test π/2 ≈ NeRCA.angle_between([1,0,0], [0,0,1])
+    @test π ≈ NeRCA.angle_between([1,0,0], [-1,0,0])
+end
 
-# azimuth()
-@test π/2 == NeRCA.azimuth(Direction(0,1,0))
-@test 0 ≈ NeRCA.azimuth(Direction(1,0,0))
-@test -π/2 ≈ NeRCA.azimuth(Direction(0,-1,0))
-@test π ≈ NeRCA.azimuth(Direction(-1,0,0))
+@testset "azimuth()" begin
+    @test π/2 == NeRCA.azimuth(Direction(0,1,0))
+    @test 0 ≈ NeRCA.azimuth(Direction(1,0,0))
+    @test -π/2 ≈ NeRCA.azimuth(Direction(0,-1,0))
+    @test π ≈ NeRCA.azimuth(Direction(-1,0,0))
+end
+
+@testset "zenith()" begin
+    @test 0.0 == NeRCA.zenith(Direction(0,0,-1))
+    @test π/2 == NeRCA.zenith(Direction(0,1,0))
+    @test π/2 == NeRCA.zenith(Direction(1,1,0))
+    @test π ≈ NeRCA.zenith(Direction(0,0,1))
+end
