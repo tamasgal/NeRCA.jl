cf4810c27ab282e8d4b1fc19d836d72958942214
Tamas Gal <tgal@km3net.de>
2019-10-09 15:50:42 +0200

    Cleanup

--- src/mc.jl
+++ src/mc.jl
@@ -1,24 +1,4 @@
 """
-    function mc_run_id(fname::AbstractString)
-
-Generate a (unique) run ID for a given filename.
-"""
-function mc_run_id(fname::AbstractString)
-    bname = basename(fname)
-    if contains(bname, "_muatm")
-        s = split(split(bname, "_muatm")[2], ".")[1]
-        energy_cut, run = split(s, "T")
-        return parse(Int, energy_cut) * 10000 + parse(Int, run)
-    end
-    if contains(bname, "_numuCC_")
-        run = split(split(bname, "_numuCC_")[2], ".")[1]
-        return parse(Int, run)
-    end
-    error("Don't know how to generate a proper run ID for '$bname'.")
-end
-
-
-"""
     function make_mc_time_converter(event_info::Union{MCEventInfo,DAQEventInfo})
 
 Returns a function which converts MC time to JTE time.
