25c818154260023b33f36e4abceb6540763a9aaf
Tamas Gal <tgal@km3net.de>
2020-06-07 17:49:56 +0200

    Update single DU reco runner

--- scripts/single_du_fit.jl
+++ scripts/single_du_fit.jl
@@ -13,8 +13,15 @@ const ROOTFILE = ARGS[2]
 function main()
     println("Starting reconstruction.")
     sparams = NeRCA.SingleDURecoParams()
-    event_hits = NeRCA.read_snapshot_hits(NeRCA.OnlineFile(ROOTFILE))
-    println(length(event_hits))
+    f = NeRCA.OnlineFile(ROOTFILE)
+    event_shits = NeRCA.read_snapshot_hits(f)
+    event_thits = NeRCA.read_triggered_hits(f)
+    for (shits, thits) in zip(event_shits, event_thits)
+        println(length(shits))
+        println(length(thits))
+        hits = NeRCA.combine(shits, thits)
+        @show hits[1:4] length(hits)
+    end
 end
 
 main()
