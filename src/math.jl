5fe1f44455de61a21dabf2d5c19277cc1ba0b806
Tamas Gal <tgal@km3net.de>
2020-06-09 09:33:40 +0200

    Loosen up dispatch constraints

--- src/math.jl
+++ src/math.jl
@@ -1,7 +1,7 @@
-Base.angle(d1::Direction, d2::Direction) = acos(min(dot(normalize(d1), normalize(d2)), 1))
+Base.angle(d1, d2) = acos(min(dot(normalize(d1), normalize(d2)), 1))
 Base.angle(a::T, b::T) where {T<:Union{CalibratedHit, PMT, MCTrack}} = Base.angle(a.dir, b.dir)
-Base.angle(a::FieldVector{3}, b::Union{CalibratedHit, PMT, MCTrack}) = Base.angle(a, b.dir)
-Base.angle(a::Union{CalibratedHit, PMT, MCTrack}, b::FieldVector{3}) = Base.angle(a.dir, b)
+Base.angle(a, b::Union{CalibratedHit, PMT, MCTrack}) = Base.angle(a, b.dir)
+Base.angle(a::Union{CalibratedHit, PMT, MCTrack}, b) = Base.angle(a.dir, b)
 
 
 """
