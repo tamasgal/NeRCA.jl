777ceebfb26fb2818690a0829182bd5dfd2b63d8
Tamas Gal <tgal@km3net.de>
2020-06-12 00:32:22 +0200

    Update single du fit script

--- scripts/single_du_fit.jl
+++ scripts/single_du_fit.jl
@@ -6,13 +6,14 @@ if length(ARGS) < 2
     exit(1)
 end
 
+const DETX = ARGS[1]
 const ROOTFILE = ARGS[2]
 
 
 function main()
     println("Starting reconstruction.")
 
-    const calib = Calibration("latest.detx")
+    calib = Calibration(DETX)
     f = NeRCA.OnlineFile(ROOTFILE)
 
     sparams = NeRCA.SingleDURecoParams()
@@ -20,7 +21,7 @@ function main()
     event_shits = NeRCA.read_snapshot_hits(f)
     event_thits = NeRCA.read_triggered_hits(f)
     for (shits, thits) in zip(event_shits, event_thits)
-        hits = calibrate(NeRCA.combine(shits, thits))
+        hits = calibrate(calib, NeRCA.combine(shits, thits))
 
 
         triggered_hits = triggered(hits)
@@ -30,6 +31,15 @@ function main()
         n_dus = length(dus)
         n_triggered_dus = length(triggered_dus)
         n_doms = length(unique(h->h.dom_id, triggered_hits))
+
+        for (idx, du) in enumerate(dus)
+            du_hits = filter(h->h.du == du, hits)
+            if length(triggered(du_hits))== 0
+                continue
+            end
+            fit = NeRCA.single_du_fit(du_hits, sparams)
+            println(fit)
+        end
     end
 end
 
