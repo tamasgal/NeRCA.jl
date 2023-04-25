6982a89a0e6e9c7b9df04da759b3f78fdeabbe37
Tamas Gal <himself@tamasgal.com>
2023-04-25 17:10:42 +0200

    Update CI

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,26 @@
 using Documenter, NeRCA
 
 makedocs(;
-    modules=[NeRCA],
-     format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
-    pages=[
+    modules = [NeRCA],
+    sitename = "NeRCA.jl",
+    authors = "Tamas Gal",
+    format = Documenter.HTML(;
+        assets = ["assets/custom.css"],
+        sidebar_sitename = false,
+        collapselevel = 4,
+        warn_outdated = true,
+    ),
+    pages = [
         "Home" => "index.md",
         "I/O" => "io.md",
         "Hits" => "hits.md",
         "ROyFit" => "fit.md",
     ],
-    repo="https://github.com/tamasgal/NeRCA.jl/blob/{commit}{path}#L{line}",
-    sitename="NeRCA.jl",
-    authors="Tamas Gal",
-    assets=String[],
+    repo = "https://git.km3net.de/tgal/NeRCA.jl/blob/{commit}{path}#L{line}",
 )
 
 deploydocs(;
-    repo="github.com/tamasgal/NeRCA.jl",
+  repo = "git.km3net.de/tgal/NeRCA.jl",
+  devbranch = "main",
+  push_preview=true
 )
