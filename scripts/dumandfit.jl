40131d27de9e6c3dba768d62997f346e0d0246e0
Tamas Gal <himself@tamasgal.com>
2023-06-16 17:40:08 +0200

    Update dumand example script

--- scripts/dumandfit.jl
+++ scripts/dumandfit.jl
@@ -1,7 +1,7 @@
 #!/usr/bin/env julia
 
-if length(ARGS) < 1
-    println("Usage: ./dumandfit.jl OFFLINEFILE")
+if length(ARGS) < 2
+    println("Usage: ./dumandfit.jl DETECTORFILE ONLINEFILE")
     exit(1)
 end
 
@@ -22,6 +22,7 @@ end
 struct RecoPerformance
     walltime::Float32
     n_dus::Int32
+    n_modules::Int32
     angular_error::Float32
     mc_zenith::Float32
     reco_zenith::Float32
@@ -30,27 +31,34 @@ struct RecoPerformance
 end
 
 function main()
-    fname = ARGS[1]
+    det = Detector(ARGS[1])
+    fname = ARGS[2]
     f = KM3io.ROOTFile(fname)
+
     outfile = H5File("test.h5", "w")
     reco_dset = create_dataset(outfile, "reco/dumand", DumandFit)
     reco_performance_dset = create_dataset(outfile, "reco_performance/dumand", RecoPerformance)
 
-    @showprogress 1 for event ∈ f.offline
-        thits = filter(triggered, event.hits)
+
+    @showprogress 1 for event ∈ f.online.events
+        thits = calibrate(det, event.triggered_hits)
 
         walltime = @elapsed track = dumandfit(thits)
 
-        ν = first(event.mc_trks)  # primary neutrino by definition the first MC track
-        ξ = angle(track.dir, ν.dir)
-        n_dus = length(unique([h.dom_id for h ∈ thits]))
+        mc_event = f.offline[event.header.trigger_counter + 1]  # one-based indexing in Julia
 
-        rp = RecoPerformance(walltime, n_dus, ξ, zenith(ν.dir), zenith(track.dir), ν.E, length(thits))
+        ν = first(mc_event.mc_trks)  # primary neutrino by definition the first MC track
+        ξ = angle(track.dir, ν.dir)
+        n_dus = length(unique([h.du for h ∈ thits]))
+        n_modules = length(unique([h.dom_id for h ∈ thits]))
+
+        rp = RecoPerformance(walltime, n_dus, n_modules, ξ, zenith(ν.dir), zenith(track.dir), ν.E, length(thits))
 
         push!(reco_dset, DumandFit(track.pos..., track.dir...))
         push!(reco_performance_dset, rp)
     end
     close(outfile)
+    close(f)
 end
 
 main()
