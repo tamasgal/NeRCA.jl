40580b29274a24700f14f4b51ad6ae432402a191
Tamas Gal <himself@tamasgal.com>
2023-09-21 11:07:37 +0200

    Remove obsolete comments

--- scripts/muonscanfit.jl
+++ scripts/muonscanfit.jl
@@ -13,21 +13,6 @@ function main()
 
     outfile = open(basename(fname) * ".txt", "w")
 
-    # event 1 (trigger_counter 4)
-    #  Direction(0.041586942949292174, -0.029550312319766795, 0.9986978047527373)
-    # event 2 (trigger_counter 5)
-    #  Direction(0.7284017634046146, -0.28083929562338567, 0.6249481267295778)
-    # event 3 (trigger_counter 17)
-    # Direction(-0.1129797622905296, -0.484448542891068, 0.8674936210736632)
-
-    # for event_id ∈ [3, 1, 2, 4, 5]  # Jpp readout order, sorted by frame_index
-    #     # println("\n\n========================================")
-    #     event = f.online.events[event_id]
-    #     # @show event_id event.header.frame_index
-    #     muons = msfit(event.snapshot_hits)
-    #     # @show muons
-    # end
-
     n_events = 0
     n_muons = 0
     n_failed = 0
