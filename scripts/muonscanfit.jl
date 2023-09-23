d6c8916da68f0bb10adcecb9555f740fbab7a509
Tamas Gal <himself@tamasgal.com>
2023-09-23 09:24:08 +0200

    Update muonscanfit progamme

--- scripts/muonscanfit.jl
+++ scripts/muonscanfit.jl
@@ -1,34 +1,92 @@
 #!/usr/bin/env julia
+doc = """Muon Scanfit
+
+Usage:
+  muonscanfit.jl -a DETECTORFILE -i INPUTFILE -o OUTPUTFILE
+  muonscanfit.jl -h | --help
+  muonscanfit.jl --version
+
+Options:
+  -a DETECTORFILE  The DETX file containing geometry and time calibration.
+  -i INPUTFILE     ROOT file containing "online" events.
+  -o OUTPUTFILE    The HDF5 file to write the output into.
+"""
+using DocOpt
+const args = docopt(doc)  # fail early ;)
+
+println("Loading libraries...")
 using NeRCA
 using KM3io
 using ProgressMeter
 
+
+struct MuonScanfitResult
+    det_id::Int
+    run::Int
+    frame_index::Int
+    trigger_counter::Int
+    pos_x::Float64
+    pos_y::Float64
+    pos_z::Float64
+    dir_x::Float64
+    dir_y::Float64
+    dir_z::Float64
+    t::Float64
+    Q::Float64
+    NDF::Int
+    walltime::Float64
+    mc_angular_error::Float64
+    mc_energy::Float64
+end
+
 function main()
-    fname = ARGS[1]
-    f = NeRCA.ROOTFile(fname)
-    det = KM3io.Detector("KM3NeT_00000133_20221025.detx")
+    f = NeRCA.ROOTFile(args["-i"])
+    det = KM3io.Detector(args["-a"])
 
     msfparams = MuonScanfitParameters(;tmaxlocal=18.0, roadwidth=200.0)
     msfit = MuonScanfit(msfparams, det)
 
-    outfile = open(basename(fname) * ".txt", "w")
+    outfile = H5File(args["-o"], "w")
+    dset = create_dataset(outfile, "reco/muonscanfit", MuonScanfitResult)
 
     n_events = 0
     n_muons = 0
     n_failed = 0
-    @showprogress 1 for (event_id, event) ∈ enumerate(f.online.events)
-        muons = msfit(event.snapshot_hits)
+    @showprogress 1 for event ∈ f.online.events
+        walltime = @elapsed muons = msfit(event.snapshot_hits)
+
         n_events += 1
         n_muons += length(muons)
+
         if length(muons) == 0
             n_failed += 1
             continue
         end
+
         best_muon = first(muons)
-        neutrino = first(f.offline[event.header.trigger_counter + 1].mc_trks)
-        angular_error = angle(best_muon.dir, neutrino.dir)
-        write(outfile, "$angular_error\n")
-        flush(outfile)
+
+        mc_angular_error = NaN
+        mc_energy = NaN
+        if f.online !== nothing
+            neutrino = first(f.offline[event.header.trigger_counter + 1].mc_trks)
+            mc_angular_error = rad2deg(angle(best_muon.dir, neutrino.dir))
+            mc_energy = neutrino.E
+        end
+
+        push!(dset, MuonScanfitResult(
+            event.header.detector_id,
+            event.header.run,
+            event.header.frame_index,
+            event.header.trigger_counter,
+            best_muon.pos...,
+            best_muon.dir...,
+            best_muon.t,
+            best_muon.Q,
+            best_muon.NDF,
+            walltime,
+            mc_angular_error,
+            mc_energy,
+        ))
     end
     close(outfile)
 
