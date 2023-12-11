9fe7ef49a1e3f52d60c4ecf117198eabba9ce9bb
Tamas Gal <himself@tamasgal.com>
2023-12-11 12:50:59 +0100

    Remove unneeded tests

--- test/math_tests.jl
+++ test/math_tests.jl
@@ -7,10 +7,6 @@ using Test
     @test π/2 ≈ angle([1,0,0], [0,1,0])
     @test π/2 ≈ angle([1,0,0], [0,0,1])
     @test π ≈ angle([1,0,0], [-1,0,0])
-
-    m1 = NeRCA.MuonScanfitCandidate(Position(1, 2, 3), Direction(1.0, 0.0, 0.0), 0, 0, 0, 0)
-    m2 = NeRCA.MuonScanfitCandidate(Position(1, 2, 3), Direction(0.0, 1.0, 0.0), 0, 0, 0, 0)
-    @test π/2 ≈ angle(m1, m2)
 end
 
 
